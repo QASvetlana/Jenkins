c73f860c91a3a7ea16dfb49026ffc00fadefeb62
Svetlana <89111378147@yandex.ru>
2021-11-01 00:21:15 +0300

    added SystemPropertiesTests

--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -41,4 +41,16 @@ public class SystemPropertiesTests {
         System.out.println(value);
         // another_value
     }
+
+    @Test
+    @Tag("properties")
+    void someTest5() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+    }
 }
