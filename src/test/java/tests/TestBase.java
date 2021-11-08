8236bb1beb7ede1307ab2cedd1f7699552a47808
Svetlana <89111378147@yandex.ru>
2021-11-08 19:57:55 +0300

    fixed

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,33 +2,51 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import helpers.Attach;
+
+
+
+
+import static java.lang.String.format;
+
+
 
 
 public class TestBase {
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+
+
     @BeforeAll
-    static void setup() {
+    public static void browserInitialConfiguration() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
+
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+//            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = format("https://%s:%s@%s", credentials.login(),
+                credentials.password(),System.getProperty("url"));
+//          gradle clean test -Durl=selenoid.autotests.cloud/wd/hub/
     }
 
+
     @AfterEach
-    public void tearDown() {
-        Attach.screenshotAs("Last screenshot");
+    public void attach() {
+        Attach.screenshotAs("Screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
     }
-
 }
